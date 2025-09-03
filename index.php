696329e9e10e71c52276cf86e549023e9a2c9a2e
whoselevenn <alois.pourny@gmail.com>
2025-09-03 22:09:04 +0200

    Découpage d'une page web

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon site de recettes</title>
+    </head>
+ 
+    <body>
+ 
+    <!-- L'en-tête -->
+    
+    <header>
+        <!-- Le menu -->
+    
+        <nav id="menu">        
+            <div class="element_menu">
+                <h3>Titre menu</h3>
+                <ul>
+                    <li><a href="page1.html">Lien</a></li>
+                    <li><a href="page2.html">Lien</a></li>
+                    <li><a href="page3.html">Lien</a></li>
+                </ul>
+            </div>    
+        </nav>
+       
+    </header>
+    
+    <!-- Le corps -->
+    
+    <div id="corps">
+        <h1>Mon site de recettes</h1>
+                
+            <p>
+                Bienvenue sur mon site de recettes !
+            </p>
+        </div>
+    
+    <!-- Le pied de page -->
+    
+    <footer id="pied_de_page">
+        <p>Copyright moi, tous droits réservés</p>
+    </footer>
+    
+    </body>
+</html>

be18a0c7066a8dcf12f47c257fd4e8bfdcb2ed77
whoselevenn <alois.pourny@gmail.com>
2025-09-03 21:42:20 +0200

    Les tableaux associatifs

--- AfficherRecettes.php
+++ AfficherRecettes.php
@@ -1,23 +1,51 @@
-<?php
-// Déclaration du tableau des recettes
-$recipes = [
-['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
-['Couscous','[...]','mickael.andrieu@exemple.com',false,],
-];
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Affichage des recettes</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <ul>
-        <?php for ($lines = 0; $lines <= 1; $lines++): ?>
-        <li>
-            <?php echo $recipes[$lines][0] . ' (' . $recipes[$lines][2] . ')';?>
-        </li>
-        <?php endfor; ?>
-    </ul>
+    <?php 
+    $recipes = [
+        [
+            'title' => 'Cassoulet',
+            'recipe' => 'Etape 1 : des flageolets',
+            'author' => 'Mickael Andrieu',
+            'is_enabled' => true,
+        ],
+        [
+            'title' => 'Escalope milanaise',
+            'recipe' => 'Etape 1 : prenez une belle escalope',
+            'author' => 'Mathieu Nebra',
+            'is_enabled' => true,
+        ],
+        [
+            'title' => 'Boeuf bourguignon',
+            'recipe' => 'Etape 1 : prenez du boeuf',
+            'author' => 'Philippe Etchebest',
+            'is_enabled' => true,
+        ],
+        [
+            'title' => 'Beignet catalan',
+            'recipe' => 'Etape 1 : prenez de beaux beignets',
+            'author' => 'Cyril Lignac',
+            'is_enabled' => true,
+        ],
+        [
+            'title' => 'Ratatouille',
+            'recipe' => 'Etape 1 : prenez de beaux légumes',
+            'author' => 'Jean-François Piège',
+            'is_enabled' => false,
+        ]
+    ];
+
+    foreach ($recipes as $recipe) {
+        if ($recipe['is_enabled']) {
+            echo '<h2>' . $recipe['title'] . '</h2>';
+            echo '<p>' . $recipe['recipe'] . '</p>';
+            echo '<p>' . $recipe['author'] . '</p>';
+        }
+    }
+    ?>
 </body>
 </html>

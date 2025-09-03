da383c6096dbcea95ac8b0b23002db41a5b983af
whoselevenn <alois.pourny@gmail.com>
2025-09-03 21:59:19 +0200

    Les fonctions

--- AfficherRecettes.php
+++ AfficherRecettes.php
@@ -6,44 +6,67 @@
 </head>
 <body>
     <?php 
+    $users = [
+        [
+            'full_name' => 'Mickaël Andrieu',
+            'email' => 'mickael.andrieu@exemple.com',
+            'age' => 34,
+        ],
+        [
+            'full_name' => 'Mathieu Nebra',
+            'email' => 'mathieu.nebra@exemple.com',
+            'age' => 34,
+        ],
+        [
+            'full_name' => 'Laurène Castor',
+            'email' => 'laurene.castor@exemple.com',
+            'age' => 28,
+        ],
+    ];
+
     $recipes = [
         [
             'title' => 'Cassoulet',
-            'recipe' => 'Etape 1 : des flageolets',
-            'author' => 'Mickael Andrieu',
+            'recipe' => '',
+            'author' => 'mickael.andrieu@exemple.com',
             'is_enabled' => true,
+        ],
+        [
+            'title' => 'Couscous',
+            'recipe' => '',
+            'author' => 'mickael.andrieu@exemple.com',
+            'is_enabled' => false,
         ],
         [
             'title' => 'Escalope milanaise',
-            'recipe' => 'Etape 1 : prenez une belle escalope',
-            'author' => 'Mathieu Nebra',
+            'recipe' => '',
+            'author' => 'mathieu.nebra@exemple.com',
             'is_enabled' => true,
         ],
         [
-            'title' => 'Boeuf bourguignon',
-            'recipe' => 'Etape 1 : prenez du boeuf',
-            'author' => 'Philippe Etchebest',
-            'is_enabled' => true,
-        ],
-        [
-            'title' => 'Beignet catalan',
-            'recipe' => 'Etape 1 : prenez de beaux beignets',
-            'author' => 'Cyril Lignac',
-            'is_enabled' => true,
-        ],
-        [
-            'title' => 'Ratatouille',
-            'recipe' => 'Etape 1 : prenez de beaux légumes',
-            'author' => 'Jean-François Piège',
+            'title' => 'Salade Romaine',
+            'recipe' => '',
+            'author' => 'laurene.castor@exemple.com',
             'is_enabled' => false,
-        ]
+        ],
     ];
+
+    function displayAuthor(string $authorEmail, array $users): string {
+        for ($i = 0; $i < count($users); $i++) {
+            $author = $users[$i];
+            if ($authorEmail === $author['email']) {
+                return $author['full_name'] . ' (' . $author['age'] . ' ans)';
+            }
+        }
+        return 'Auteur inconnu';
+    }
+
+    echo '<h1>Recettes de cuisine</h1>';    
 
     foreach ($recipes as $recipe) {
         if ($recipe['is_enabled']) {
             echo '<h2>' . $recipe['title'] . '</h2>';
-            echo '<p>' . $recipe['recipe'] . '</p>';
-            echo '<p>' . $recipe['author'] . '</p>';
+            echo '<p>' . displayAuthor($recipe['author'], $users) . '</p>';
         }
     }
     ?>

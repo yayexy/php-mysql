e317faacf89526eec49d56b481ba2744b211e809
whoselevenn <alois.pourny@gmail.com>
2025-09-23 10:23:55 +0200

    Connectez PHP à MySQL avec PDO

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
   <div class="container">
-    <a class="navbar-brand fw-bold" href="#">Site de recettes</a>
+    <a class="navbar-brand fw-bold" href="index.php">Site de recettes</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
